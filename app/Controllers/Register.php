a3b814eeeca5a62b4b019e0a29cb556400a7ea11
elbarbaro <leopoldo.misael.lopez.de.alba@gmail.com>
2021-04-04 13:27:51 -0500

    New user created in database with sigunup method

--- app/Controllers/Register.php
+++ app/Controllers/Register.php
@@ -3,6 +3,7 @@
 
 namespace App\Controllers;
 
+use App\Entities\User;
 
 class Register extends BaseController
 {
@@ -11,5 +12,22 @@ class Register extends BaseController
         echo view('include_files/navbar');
         echo view('registro');
         echo view('include_files/footer');
+    }
+
+    public function create() {
+        $data = [
+            'first_name' => $this->request->getPost('first_name'),
+            'last_name' => $this->request->getPost('last_name'),
+            'birthdate' => $this->request->getPost('birthdate'),
+            'genre' => $this->request->getPost('genre'),
+            'username' => $this->request->getPost('username'),
+            'email' => $this->request->getPost('email'),
+            'password' => $this->request->getPost('password')
+        ];
+        $user = new User();
+        $user->fill($data);
+        $userModel = model('App\Models\UserModel', false);
+        $userModel->save($user);
+        $this->response->redirect('/');
     }
 }
