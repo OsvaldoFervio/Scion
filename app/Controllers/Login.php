6b9a5656d85527ff966456a2f85b6e0b202c3a5f
elbarbaro <leopoldo.misael.lopez.de.alba@gmail.com>
2021-04-04 13:01:47 -0500

    Removed register route in the Login controller

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -8,17 +8,17 @@ class Login extends BaseController
 {
 	public function index()
 	{
-		echo view('include_files/header');
+	    echo view('include_files/header');
 		echo view('include_files/navbar');
 		echo view('login');
 		echo view('include_files/footer');
 	}
 
-    public function registro()
-    {	
-		echo view('include_files/header');
-		echo view('include_files/navbar');
-		echo view('registro');	
-		echo view('include_files/footer');
-	}
+	public function post() {
+	    $data = [
+	        'email' => $this->request->getPost('email'),
+            'password' => $this->request->getPost('password')
+        ];
+
+    }
 }
