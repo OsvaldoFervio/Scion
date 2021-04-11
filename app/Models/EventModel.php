b5029c61984c6b210edc8bf7d920d41a90b7aeeb
elbarbaro <leopoldo.misael.lopez.de.alba@gmail.com>
2021-04-11 17:43:30 -0500

    Fetch event type data and country in the model

--- app/Models/EventModel.php
+++ app/Models/EventModel.php
@@ -34,4 +34,34 @@ class EventModel extends Model
     protected $useTimestamps = true;
     protected $useSoftDeletes = true;
 
+    protected $afterFind = ['getTypeName'];
+
+    protected function getTypeName(array $data) {
+        if(isset($data['id'])) {
+            $item = $data['data'];
+            $this->bindEventTypeData($item);
+        } else {
+            foreach ($data['data'] as $item) {
+                $this->bindEventTypeData($item);
+            }
+        }
+        return $data;
+    }
+
+    protected function bindEventTypeData($item) {
+        $eventTypeModel = \model('EventTypeModel');
+        if(isset($item->type_id)){
+            if ($item->type_id == 3 && $item->country_id) {
+                $item->country_name = $this->getCountryName($item->country_id);
+            } else {
+                $typeName = $eventTypeModel->find($item->type_id)->name;
+                $item->type_name = $typeName;
+            }
+        }
+    }
+
+    protected function getCountryName($id) {
+        $countryModel = \model('CountryModel');
+        return $countryModel->find($id)->name;
+    }
 }
