7c0c15766ac871d8466678e807f8aca15adbb7bb
Osvaldo Fervio <fervio.system@gmail.com>
2021-05-05 23:18:38 -0500

    contruccion dashboard

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost/ScionsGit';
+	public $baseURL = 'http://localhost/Scion/';
 
 	/**
 	 * --------------------------------------------------------------------------
---
+++ app/Views/Admin/index.php
@@ -0,0 +1,116 @@
+<!-- page content -->
+<div class="right_col" role="main">
+  <div class="">
+  <div class="row" style="display: inline-block;" >
+  <div class="tile_count">
+    <div class="col-md-3 col-sm-4  tile_stats_count">
+      <span class="count_top"><i class="fa fa-users"></i> Total de Alumnos</span>
+      <div class="count"></div>
+      <span class="count_bottom"><i class="green">Registrados </i></span>
+    </div>
+    <!-- <div class="col-md-2 col-sm-4  tile_stats_count">
+      <span class="count_top"><i class="fa fa-users"></i> Total de aspirantes</span>
+      <div class="count">30</div>
+      <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> Última semana</span>
+    </div> -->
+    <div class="col-md-3 col-sm-4  tile_stats_count">
+      <span class="count_top"><i class="fa fa-user"></i> Total Fichas de Pago</span>
+      <div class="count green"></div>
+      <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>Enviadas</i></span>
+    </div>
+    <div class="col-md-2 col-sm-4  tile_stats_count">
+      <span class="count_top"><i class="fa fa-user"></i> Total Pendientes Pagar</span>
+      <div class="count"></div>
+        <span class="count_bottom"><i class="green"><i class="fa fa-sort-desc"></i>Por pagar </i></span>
+    </div>
+    <div class="col-md-2 col-sm-4  tile_stats_count">
+      <span class="count_top"><i class="fa fa-user"></i> Total Pendientes Validar</span>
+      <div class="count"></div>
+      <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>Por validar </i></span>
+    </div>
+    <div class="col-md-2 col-sm-4  tile_stats_count">
+      <span class="count_top"><i class="fa fa-user"></i> Total Pagos Validados</span>
+      <div class="count"></div>
+      <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i> Verificados</i></span>
+    </div>
+    
+  </div>
+  </div>
+    <div class="row">
+      <div class="col-md-12 col-sm-12 ">
+        <div class="x_panel">
+          <div class="x_title">
+            <div class="col-md-6">
+              <h3>Alumnos <small> Reinscripcion</small></h3>
+            </div>
+            <div class="col-md-6">
+            <ul class="nav navbar-right panel_toolbox">
+              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Settings 1</a>
+                    <a class="dropdown-item" href="#">Settings 2</a>
+                  </div>
+              </li>
+              <li><a class="close-link"><i class="fa fa-close"></i></a>
+              </li>
+            </ul>
+          </div>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="card-box table-responsive">
+                  <p class="text-muted font-13 m-b-30">
+                    Busqueda y filtrado de información. Los resultados se podrán exportar en el formato seleccionado.
+                  </p>          
+                  <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+                    <thead>
+                      <tr>                        
+                        <th>CURP</th>
+                        <th>Nombre</th>
+                        <th>Apellidos</th>
+                        <th>Carrera</th>
+                        <th>Turno</th>
+                        <th>Grupo</th>                        
+                        <th>Ficha Pago</th>
+                        <th>Pago</th>
+                        <th>Ficha</th>
+                        <th>Acción</th>
+                        <th>Rechazar</th>
+                      </tr>
+                    </thead>
+
+                
+
+
+                    <tbody>
+                        <tr>                        
+                            <th>CURP</th>
+                            <th>Nombre</th>
+                            <th>Apellidos</th>
+                            <th>Carrera</th>
+                            <th>Turno</th>
+                            <th>Grupo</th>                        
+                            <th>Ficha Pago</th>
+                            <th>Pago</th>
+                            <th>Ficha</th>
+                            <th>Acción</th>
+                            <th>Rechazar</th>
+                          </tr>
+                      
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>          
+    </div>
+  </div>
+</div>
+<!-- /page content -->
