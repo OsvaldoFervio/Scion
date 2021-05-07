06289fdfa0d72a8585c7f66b871f00a114c3df9a
PIMIENTA VARGAS CARLOS <desarrollo@eze-trip.com>
2021-05-07 12:52:19 -0500

    Dashboard

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -7,34 +7,24 @@ use App\Models\UserModel;
 class Dashboard extends BaseController
 {
     protected $service = null;
-
     
-
-  public function index()
-    {
-     
-                 //$vista = $this->template('auth/login, $data'); 
+    public function index(){     
+        //$vista = $this->template('auth/login, $data'); 
         
-
         echo view('Admin/head');
-        echo view('Admin/leftnav');  
-        echo view('Admin/nav');  
         echo view('Admin/index');
         echo view('Admin/footer');
 
         // echo view('Dashboard/index',array('data'=>$data ,'totales'=>$totales),TRUE);
-    }
-   
-
-  
- public function getTemplate($view){
-        $data = array(
+    }  
+    public function getTemplate($view){
+        /*$data = array(
             'head' => $this->load->view('layout/head','',TRUE), 
             'leftnav' => $this->load->view('layout/leftnav','',TRUE),
             'nav' => $this->load->view('layout/nav','',TRUE),
             'content' => $view,
             'footer' => $this->load->view('layout/footer','',TRUE),
         );
-        $this->load->view('dashboard',$data);
+        $this->load->view('dashboard',$data);*/
     }
 }
