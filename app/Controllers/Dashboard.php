7016410a9bb184af1bc9d4c32d3e3eee364f9bfc
Osvaldo Fervio <fervio.system@gmail.com>
2021-05-13 17:29:27 -0500

    Avances dashboard eventos

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -3,20 +3,58 @@ namespace App\Controllers;
 
 use CodeIgniter\Controller;
 use App\Models\UserModel;
+use App\Controllers\BaseController;
+use Config\Services;
 
 class Dashboard extends BaseController
 {
     protected $service = null;
+
+    public function __construct()
+    {
+        $this->service = service('event');
+    }
     
     public function index(){     
         //$vista = $this->template('auth/login, $data'); 
-        
+        $modelEvent = model('EventModel');
+        $events = $modelEvent->orderBy('created_at', 'desc')
+                             ->paginate();
+                             
         echo view('Admin/head');
+        echo view('Admin/leftnav');
         echo view('Admin/index');
         echo view('Admin/footer');
 
         // echo view('Dashboard/index',array('data'=>$data ,'totales'=>$totales),TRUE);
-    }  
+    }
+
+    public function Event(){     
+        //$vista = $this->template('auth/login, $data'); 
+         $data = $this->getData();
+        
+        echo view('Admin/head');
+        echo view('Admin/leftnav');
+        echo view('Admin/event_form',$data);
+        echo view('Admin/footer');
+
+        // echo view('Dashboard/index',array('data'=>$data ,'totales'=>$totales),TRUE);
+    }
+
+    public function createEvent() {
+        $data = $this->request->getPost();
+        var_dump($data);
+        //return;
+        if($this->validate('event')) {
+            $eventId = $this->service->create($data);
+            $this->service->storeEventImages($eventId, $this->request->getFiles());
+            return redirect()->to(base_url('Dashboard/Event'))->with('success', 'Evento creado');
+        } else {
+            return redirect()->back()->with('errors', $this->validator->getErrors());
+        }
+    }
+
+
     public function getTemplate($view){
         /*$data = array(
             'head' => $this->load->view('layout/head','',TRUE), 
@@ -26,5 +64,39 @@ class Dashboard extends BaseController
             'footer' => $this->load->view('layout/footer','',TRUE),
         );
         $this->load->view('dashboard',$data);*/
+    }
+
+    protected function getData() {
+        $modelEventType = model('EventTypeModel');
+        $modelCategory = model('CategoryModel');
+        $modelDifficulty = model('DifficultyModel');
+        $modelPlatform = model('PlatformModel');
+        $modelVideogame = model('VideogameModel');
+        $modelCountry = model('CountryModel');
+        $modelTimezone = model('TimeZoneModel');
+        $modelCurrency = model('CurrencyModel');
+
+
+        $eventTypes = $modelEventType->findAll();
+        $countries = $modelCountry->findAll();
+        $categories = $modelCategory->findAll();
+        $difficulties = $modelDifficulty->findAll();
+        $videogames = $modelVideogame->findAll();
+        $platforms = $modelPlatform->findAll();
+        $timezones = $modelTimezone->findAll();
+        $currencies = $modelCurrency->findAll();
+
+        $data = [
+            'eventTypes' => $eventTypes,
+            'countries' => $countries,
+            'categories' => $categories,
+            'difficulties' => $difficulties,
+            'videogames' => $videogames,
+            'platforms' => $platforms,
+            'timezones' => $timezones,
+            'currencies' => $currencies,
+        ];
+
+        return $data;
     }
 }
