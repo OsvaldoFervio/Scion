420042c8bdc2228a46abeb4e6cf886c69edaa36b
elbarbaro <leopoldo.misael.lopez.de.alba@gmail.com>
2021-06-06 14:32:52 -0500

    Fix HTML element for videogames total number

--- app/Views/Admin/games.php
+++ app/Views/Admin/games.php
@@ -1,65 +1,68 @@
-<div class="right_col" role="main" style="min-height: 941px;">
-    <div>
-        <div class="row" style="display: inline-block;">
-            <div class="col-md-12 col-sm-12">
-                <div class="tile_count">
-                <div class="col-md-3 col-sm-4  tile_stats_count">
-                    <span class="count_top"><i class="fa fa-user"></i> Total Juegos</span>
-                    <div class="count">250</div>
-                </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12 col-sm-12 ">
-                <div class="x_panel">
-                    <div class="x_title">
-                        <div class="col-md-6">
-                            <h3>Juegos<small> Listado</small></h3>
+            <div class="right_col" role="main" style="min-height: 941px;">
+                <div>
+                    <div class="row" style="display: inline-block;">
+                        <div class="col-md-12 col-sm-12">
+                            <div class="tile_count">
+                                <div class="tile_stats_count">
+                                    <span class="count_top"><i class="fa fa-user"></i> Total Juegos</span>
+                                    <div class="count">250</div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="clearfix"></div>
                     </div>
-                    <div class="x_content">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="card-box table-responsive">
-                                    <p class="text-muted font-13 m-b-30">
-                                        Busqueda y filtrado de información. Los resultados se podrán exportar en el formato seleccionado.
-                                    </p>
-                                    <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
-                                        <thead>
-                                            <tr>
-                                                <th>Id</th>
-                                                <th>Nombre</th>
-                                                <th>Activo</th>
-                                                <th>Creado</th>
-                                                <th>Acciones</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php foreach($videogames as $videogame) : ?>
-                                            <tr>
-                                                <td><?= $videogame->id ?></td>
-                                                <td><?= $videogame->name ?></td>
-                                                <td>
-                                                    <?php if($videogame->active == 1) : ?>
-                                                    Activo
-                                                    <?php else: ?>
-                                                    Bloqueado
-                                                    <?php endif; ?>
-                                                </td>
-                                                <td><?= $videogame->created_at ?></td>
-                                                <td>
-                                                    <span class="count_top"><a href="#"><i class="fa fa-eye"></i>Ver</a></span>
-                                                    <span class="count_top"><a href="#"><i class="fa fa-edit"></i>Editar</a></span>
-                                                    <form>
-                                                        <button type="submit"><i class="fa fa-close"></i>Eliminar</a>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                            <?php endforeach; ?>
-                                        </tbody>
-                                    </table>
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12 ">
+                            <div class="x_panel">
+                                <div class="x_title">
+                                    <div class="col-md-6">
+                                        <h3>Juegos<small> Listado</small></h3>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <div class="x_content">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="card-box table-responsive">
+                                                <p class="text-muted font-13 m-b-30">
+                                                    Busqueda y filtrado de información. Los resultados se podrán exportar en el formato seleccionado.
+                                                </p>
+                                                <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Id</th>
+                                                            <th>Nombre</th>
+                                                            <th>Activo</th>
+                                                            <th>Creado</th>
+                                                            <th>Acciones</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <?php foreach($videogames as $videogame) : ?>
+                                                        <tr>
+                                                            <td><?= $videogame->id ?></td>
+                                                            <td><?= $videogame->name ?></td>
+                                                            <td>
+                                                                <?php if($videogame->active == 1) : ?>
+                                                                Activo
+                                                                <?php else: ?>
+                                                                Bloqueado
+                                                                <?php endif; ?>
+                                                            </td>
+                                                            <td><?= $videogame->created_at ?></td>
+                                                            <td>
+                                                                <span class="count_top"><a href="#"><i class="fa fa-eye"></i>Ver</a></span>
+                                                                <span class="count_top"><a href="#"><i class="fa fa-edit"></i>Editar</a></span>
+                                                                <form>
+                                                                    <button type="submit"><i class="fa fa-close"></i>Eliminar</a>
+                                                                </form>
+                                                            </td>
+                                                        </tr>
+                                                        <?php endforeach; ?>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -68,4 +71,4 @@
             </div>
         </div>
     </div>
-</div>
+</body>
