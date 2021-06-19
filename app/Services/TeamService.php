5b092b298e51a2c5684aa83cc0927729d8cf87ba
elbarbaro <leopoldo.misael.lopez.de.alba@gmail.com>
2021-06-19 13:38:31 -0500

    Team members created in the database

--- app/Services/TeamService.php
+++ app/Services/TeamService.php
@@ -5,14 +5,58 @@ namespace App\Services;
 class TeamService
 {
     protected $model = null;
+    protected $teamMemberModel = null;
 
     public function __construct()
     {
         $this->model = model('TeamModel');
+        $this->teamMemberModel = model('TeamMemberModel');
     }
 
     public function create($data)
     {
-        $this->model->save($data);
+        $teamData = $this->buildTeamData($data);
+        $this->model->save($teamData);
+        $teamId = $this->model->insertID;
+        echo var_dump($teamId);
+
+        $managerId = $data['manager_id'];
+        $participants = $data['user_id']; // array
+        $this->createMembers($teamId, $managerId, $participants);
+    }
+
+    private function createMembers($teamId, $managerId, $participants)
+    {
+        $managerData = [
+            [
+                'team_id' => $teamId,
+                'user_id' => $managerId,
+                'member_type_id' => 1
+            ]
+        ];
+        $data = array_merge($managerData, $this->buildParticipantsData($teamId, $participants));
+        $this->teamMemberModel->insertBatch($data);
+    }
+
+    private function buildTeamData($data)
+    {
+        return [
+            'country_id' => $data['country_id'],
+            'name' => $data['name'],
+            'discord_url' => $data['discord_url'],
+            'whatsapp_number' => $data['whatsapp_number'],
+            'email' => $data['email']
+        ];
+    }
+
+    private function buildParticipantsData($teamId, $participants)
+    {
+        return array_map(function($item) use ($teamId){
+            return [
+                'team_id' => $teamId,
+                'user_id' => $item,
+                'member_type_id' => 2
+            ];
+        }, $participants);
     }
 }
